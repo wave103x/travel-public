2554b1126b4b91ba7381f02aaf9e1d811a1e59ea
wave103 <wave103x@gmail.com>
2022-07-20 14:25:08 +0300

    feat: make popup behavior

--- travel/index.js
+++ travel/index.js
@@ -1,5 +1,3 @@
-const hamburger = document.querySelector('#hamburger');
-const menu = document.querySelector('.adapt-menu');
 const close = document.querySelector('#close-icon');
 const links = document.querySelector('.ad-links');
 
@@ -130,6 +128,7 @@ arrowR.addEventListener('click', () => {
 
 window.addEventListener('resize', () => {
     sliderLine.style.setProperty('--moveR', - imageSliderOne.clientWidth - (window.innerWidth / 100 * 3) + 'px');
+    sliderLine.style.setProperty('--moveL', imageSliderOne.clientWidth + (window.innerWidth / 100 * 3) + 'px');
 })
 
 
@@ -140,14 +139,48 @@ const popup = document.querySelector('.pop-up');
 const popupContent = document.querySelector('.pop-up__content');
 
 btnLogin.addEventListener('click', () => {
-    popupContent.classList.toggle('pop-up__active');
     popup.classList.toggle('hidden');
-
+    popupContent.classList.toggle('pop-up__active');
 });
 
-window.addEventListener('click', e => {
-    if (e.target !== popupContent && e.target !== btnLogin) {
-        popupContent.classList.remove('pop-up__active');
-        popup.classList.add('hidden');
+popup.addEventListener('click', e => {
+    if (!popupContent.contains(e.target) && !linkRegister.contains(e.target)) {
+        popup.classList.toggle('hidden');
+        popupContent.classList.toggle('pop-up__active');
     }
-})
+});
+
+const accountLink = () => {
+    popup.classList.toggle('hidden');
+    popupContent.classList.toggle('pop-up__active');
+}
+
+const linkRegister = document.querySelector('.linkRegister');
+const createLinkLogin = document.createElement('a');
+createLinkLogin.innerHTML = '<a class="linkLogin" href="#">Log in</a>'
+
+linkRegister.addEventListener('click', () => {
+    document.querySelector('.btnFace').classList.toggle('hide-popup-fields');
+    document.querySelector('.btnGoogle').classList.toggle('hide-popup-fields');
+    document.querySelector('.hr').classList.toggle('hide-popup-fields');
+    document.querySelector('.linkForgot').classList.toggle('hide-popup-fields');
+    if (linkRegister.textContent === 'Log In') {
+        document.querySelector('.header-login').textContent = 'Log in to your account';
+        document.querySelector('.btnSign').value = 'Sign In';
+        document.querySelector('.textToRegister').textContent = 'Don’t have an account? ';
+        linkRegister.textContent = 'Register';
+    } else {
+        document.querySelector('.header-login').textContent = 'Create account';
+        document.querySelector('.btnSign').value = 'Sign Up';
+        document.querySelector('.textToRegister').textContent = 'Already have an account? ';
+        linkRegister.textContent = 'Log In';
+    }
+});
+
+
+const email = document.querySelector('.form__sign-in input[name="e-mail"]');
+const pass = document.querySelector('.form__sign-in input[name="password"]');
+
+document.querySelector('.btnSign').addEventListener('click', () => {
+    alert(`Your email: ${email.value}\nYour password: ${pass.value}`);
+});
